d5b8a4f8bb40e9b8dfafb58cdf57429e99f3bf7e
supriya <angalwarsupriya@gmail.com>
2025-02-22 15:47:58 +0530

    added call and sign in

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import './index.css'; // Import the CSS file
-
+import {IoIosCall} from 'react-icons/io'
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -14,13 +14,34 @@ const Header = () => {
     <div className="header-container">
       <div className="logo">Better.com</div>
       <div className="menu-icon" onClick={toggleMenu}>
-        ☰
+        <div className='' style={{ display: "flex", alignItems: "center" }}>
+          <button className='btn'>
+          ☰
+          </button>
+
+          <div className='call-row'>
+            <div className='call-round'>
+              <IoIosCall/>
+            </div>
+            <button className='sign-in-btn'>
+              sign in
+            </button>
+          </div>
+        </div>
       </div>
       <div className={`nav-links ${isOpen ? 'open' : ''}`}>
         <Link href="/">Home</Link>
         <Link href="/about-us">About Us</Link>
         <Link href="/mortgage-calculator">Mortgage Calculator</Link>
         <Link href="/start">Get Started</Link>
+      </div>
+      <div className='call-row2'>
+          <div className='call-round'>
+             <IoIosCall/>
+          </div>
+          <button className='sign-in-btn'>
+            sign in
+          </button>
       </div>
     </div>
   );
